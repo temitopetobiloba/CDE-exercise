2757afea38f7d0c30bf302adc22538f22a2c663c
Oluwatobi Owamokele <tootees@Oluwatobis-MacBook-Air.local>
2026-09-09 09:55:45 +0200

    Run this script to update data at 12am

--- mycommands.sh
+++ mycommands.sh
@@ -3,6 +3,15 @@ mkdir raw # Create a directory named 'raw'
 
 ls ~/Downloads
 
+cd raw # Change the current directory to 'raw'
+
+curl -O https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2023-financial-year-provisional/Download-data/annual-enterprise-survey-2023-financial-year-provisional.csv
+
+nano annual-enterprise-survey-2023-financial-year-provisional.csv # Open the CSV file in nano editor for review
+
+awk -F, 'NR==1 { gsub(/Variable_code/, "variable_code") } { print $0 }' annual-enterprise-survey-2023-financial-year-provisional.csv > temp.csv && mv temp.csv annual-enterprise-survey-2023-financial-year-provisional.csv
+
+git 
 
 mv ~/Downloads/annual-enterprise-survey-2023-financial-year-provisional.csv raw/ # Move the CSV file to the 'raw' directory
 git status # Check the status of the git repository
@@ -21,3 +30,14 @@ ls Transformed/ # List the contents of the 'Transformed' directory
 git commit -m "Add transformed data 2023 _year_finance data" # Commit the changes with a message
 git add . # Stage all changes in the current directory for commit 
 git status # Check the status of the git repository
+git push # Push the committed changes to the remote repository
+mkdir Gold # Create a folder named 'Gold'
+git status # Check the status of the git repository
+mv /Transformed 2023_year_finance.csv Gold/ # Move the transformed file to the 'Gold' directory
+git status # Check the status of the git repository
+git add Gold/2023_year_finance.csv # Stage the file in the 'Gold'
+ls Gold/ # List the contents of the 'Gold' directory to confirm the file is moved here
+git add . # Stage all changes in the current directory for commit
+git status # Check the status of the git repository
+git commit -m "Add Gold data 2023_year_finance data" # Commit the changes with a message
+git push # Push the committed changes to the remote repository
